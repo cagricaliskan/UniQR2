f6ecd561d64213e88f19561094700e1ca8045cf5
Cagri Caliskan <caliskan1998@gmail.com>
2021-02-18 10:10:47 +0300

    Add project files.

---
+++ UniQR2/Models/ModelContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniQR2.Models
+{
+    public class ModelContext : DbContext 
+    {
+        public ModelContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
