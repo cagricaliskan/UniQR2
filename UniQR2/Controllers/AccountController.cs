b8837ba9d3bc4ce12436babf4309c4e972c1f0d1
Cagri Caliskan <caliskan1998@gmail.com>
2021-02-18 10:34:39 +0300

    Login Test

---
+++ UniQR2/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UniQR2.Models;
+
+namespace UniQR2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ModelContext db;
+
+        public AccountController(ModelContext db)
+        {
+            this.db = db;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(User userLoginModel)
+        {
+            User u = db.Users.FirstOrDefault(x => x.Email == userLoginModel.Email && x.Password == userLoginModel.Password);
+            if (u != null)
+            {
+                return RedirectToAction("privacy", "Home");
+            }
+            return RedirectToAction("register", "account");
+        }
+    }
+}
