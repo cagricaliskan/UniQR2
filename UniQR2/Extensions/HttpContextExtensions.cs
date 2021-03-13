9cec278f0e2e827d96f00ac1a46d0905090a3767
Cagri Caliskan <caliskan1998@gmail.com>
2021-03-13 04:01:07 +0300

    ActiveBar

--- UniQR2/Extensions/HttpContextExtensions.cs
+++ UniQR2/Extensions/HttpContextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,22 @@ namespace UniQR2.Extensions
         internal static void Configure(IHttpContextAccessor contextAccessor)
         {
             m_httpContextAccessor = contextAccessor;
+        }
+    }
+
+    public static class MvcExtensions
+    {
+        public static string ActiveClass(this IHtmlHelper htmlHelper , string actions = null , string controllers = null , string cssClass = "active")
+        {
+            var currentController = htmlHelper?.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper?.ViewContext.RouteData.Values["action"] as string;
+
+            var acceptedControllers = (controllers ?? currentController ?? "").Split(',');
+            var acceptedActions = (actions ?? currentAction ?? "").Split(',');
+
+            return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+                ? cssClass
+                : "";
         }
     }
 }
