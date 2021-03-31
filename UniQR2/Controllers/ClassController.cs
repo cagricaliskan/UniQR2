50d16cbea409f30cb2e508afb5e151ef55ddbe61
Cagri Caliskan <caliskan1998@gmail.com>
2021-03-31 19:51:24 +0300

    ClassInit

---
+++ UniQR2/Controllers/ClassController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniQR2.Models;
+using X.PagedList;
+
+namespace UniQR2.Controllers
+{
+    [Authorize("Administrator")]
+    public class ClassController: Controller
+    {
+        private readonly ModelContext db;
+
+        public ClassController(ModelContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index(int page = 1, string search = "")
+        {
+            var courseclassroom = db.CourseClassrooms.Include(x => x.Instructor).AsQueryable();
+            if(search != "")
+            {
+                courseclassroom = db.CourseClassrooms.Where(x => x.Instructor.FullName.Contains(search));
+
+                ViewBag.search = search;
+                ViewBag.count = courseclassroom.Count();
+            }
+
+            courseclassroom = courseclassroom.OrderByDescending(x => x.CourseClassroomID);
+            ViewBag.page = page;
+            return View(courseclassroom.ToPagedList(page,10));
+        }
+
+    }
+}
