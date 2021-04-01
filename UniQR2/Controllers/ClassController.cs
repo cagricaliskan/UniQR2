1d142b00f962d99172e99deea3748b48db32bc56
Cagri Caliskan <caliskan1998@gmail.com>
2021-04-01 09:20:58 +0300

    class frontend

--- UniQR2/Controllers/ClassController.cs
+++ UniQR2/Controllers/ClassController.cs
@@ -22,7 +22,9 @@ namespace UniQR2.Controllers
 
         public IActionResult Index(int page = 1, string search = "")
         {
-            var courseclassroom = db.CourseClassrooms.Include(x => x.Instructor).AsQueryable();
+            var courseclassroom = db.CourseClassrooms.Include(x => x.Instructor ).AsQueryable();
+            courseclassroom = db.CourseClassrooms.Include(n => n.Classroom).AsQueryable();
+            courseclassroom = db.CourseClassrooms.Include(z => z.Course).AsQueryable();
             if(search != "")
             {
                 courseclassroom = db.CourseClassrooms.Where(x => x.Instructor.FullName.Contains(search));
@@ -33,6 +35,13 @@ namespace UniQR2.Controllers
 
             courseclassroom = courseclassroom.OrderByDescending(x => x.CourseClassroomID);
             ViewBag.page = page;
+
+
+            ViewBag.classroom = db.Classrooms.ToList();
+            ViewBag.course = db.Courses.ToList();
+            ViewBag.ins = db.Users.Where(x => x.UserRole == UserRole.Instructor).ToList();
+
+
             return View(courseclassroom.ToPagedList(page,10));
         }
 
