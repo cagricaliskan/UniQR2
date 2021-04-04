f96554b768dc7b377f3a374ca6a41e81e36710da
Cagri Caliskan <caliskan1998@gmail.com>
2021-04-04 17:44:41 +0300

    cours-class list

--- UniQR2/Controllers/MyClassesController.cs
+++ UniQR2/Controllers/MyClassesController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using UniQR2.Models;
 using X.PagedList;
@@ -14,18 +16,24 @@ namespace UniQR2.Controllers
     public class MyClassesController : Controller
     {
         private readonly ModelContext db;
+        
+        
 
         public MyClassesController(ModelContext db)
         {
             this.db = db;
+            
         }
+
+
 
         public IActionResult Index(int page = 1, string search = "")
         {
-            var myclass = db.CourseClassrooms.Include(x => x.Classroom).AsQueryable();
-            myclass = db.CourseClassrooms.Include(n => n.Course).AsQueryable();
+
+            var myclass = db.CourseClassrooms.Include(x => x.Course).AsQueryable();
+            myclass = db.CourseClassrooms.Include(n => n.Classroom).AsQueryable();
             
-            if(search != "")
+            if (search != "")
             {
                 myclass = db.CourseClassrooms.Where(x => x.Course.Code.Contains(search) || x.Classroom.Name.Contains(search));
 
@@ -35,6 +43,9 @@ namespace UniQR2.Controllers
 
             myclass = myclass.OrderByDescending(n => n.InstructorID);
             ViewBag.page = page;
+
+            ViewBag.classroom = db.Classrooms.ToList();
+            ViewBag.course = db.Courses.ToList();
 
             return View(myclass.ToPagedList(page,10));
         }
